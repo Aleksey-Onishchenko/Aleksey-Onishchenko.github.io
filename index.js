a0576dad302a6a53c19c1714c6b25bf4e1ba5ff0
Aleksey-Onishchenko <52916511+Aleksey-Onishchenko@users.noreply.github.com>
2020-02-17 17:32:54 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,106 @@
+class Slider {
+  constructor(id) {
+
+    this.sliderElement = document.getElementById(id);
+    this.slidesElements = document.querySelectorAll('#slider img');
+
+    this.currentSlide = 0;
+    this.slidersLength = this.slidesElements.length;
+    this.slides = this.sliderElement.children;
+
+    this.addControls();
+    this.addDots();
+    this.addActiveClassForSlides();
+    this.addActiveClassForDots();
+  }
+
+
+  addDots() {
+    const divForDots = document.createElement('div');
+    divForDots.className = 'dots';
+    this.sliderElement.append(divForDots);
+    for (let i = 0; i < this.slidersLength; i += 1) {
+      const dot = document.createElement('button');
+      dot.className = 'dot';
+      divForDots.append(dot);
+    }
+    this.dots = divForDots.children;
+
+    this.addEventsForDots();
+  }
+
+  addControls() {
+    if (this.slidersLength !== 0) {
+      this.nextImg = document.createElement('button');
+      this.prevImg = document.createElement('button');
+      this.nextImg.className = 'go_next';
+      this.prevImg.className = 'go_back';
+      this.prevImg.innerHTML = '&#706';
+      this.nextImg.innerHTML = '&#707';
+      this.sliderElement.append(this.prevImg);
+      this.sliderElement.append(this.nextImg);
+      this.addEventsForControls();
+    }
+    return null;
+  }
+
+
+  goBack() {
+    if (this.currentSlide === 0) {
+      this.currentSlide = this.slidersLength - 1;
+    } else {
+      this.currentSlide -= 1;
+    }
+
+    this.addActiveClassForSlides();
+    this.addActiveClassForDots();
+  }
+
+  goNext() {
+    if (this.currentSlide === this.slidersLength - 1) {
+      this.currentSlide = 0;
+    } else {
+      this.currentSlide += 1;
+    }
+
+    this.addActiveClassForSlides();
+    this.addActiveClassForDots();
+  }
+
+
+  goTo(number) {
+    this.currentSlide = number;
+
+    this.addActiveClassForSlides();
+    this.addActiveClassForDots();
+  }
+
+  addEventsForDots() {
+    for (let i = 0; i < this.slidersLength; i += 1) {
+      this.dots[i].addEventListener('click', this.goTo.bind(this, i));
+    }
+  }
+
+  addEventsForControls() {
+    this.nextImg.addEventListener('click', this.goNext.bind(this));
+    this.prevImg.addEventListener('click', this.goBack.bind(this));
+  }
+
+  addActiveClassForDots() {
+    for (let i = 0; i < this.slidersLength; i += 1) {
+      this.dots[i].classList.remove('active');
+    }
+    if (this.slidersLength !== 0) {
+      this.dots[this.currentSlide].classList.add('active');
+    } return null;
+  }
+
+  addActiveClassForSlides() {
+    for (let i = 0; i < this.slidersLength; i += 1) {
+      this.slides[i].classList.remove('active');
+    }
+    this.slides[this.currentSlide].classList.add('active');
+  }
+}
+
+new Slider('slider');
