849ef17b46b65601e2ace579dde6835bde466633
Aleksey-Onishchenko <52916511+Aleksey-Onishchenko@users.noreply.github.com>
2020-02-18 18:32:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -100,10 +100,10 @@ class Slider {
 
     this.sliderElement.addEventListener('touchend', (event) => {
       touchendX = event.changedTouches[0].screenX;
-      if (touchendX + 50 <= touchstartX) {
+      if (touchendX <= touchstartX) {
         this.goNext();
       }
-      if (touchendX + 50 >= touchstartX) {
+      if (touchendX >= touchstartX) {
         this.goBack();
       }
     }, false);
